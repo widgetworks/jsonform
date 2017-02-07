0ddf261d686b3c4582dee08de163972b0db3df54
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2017-02-07 12:50:49 +1000

    refactor(src): move copyright comment to top

--- src/jsonform.ts
+++ src/jsonform.ts
@@ -1,27 +1,3 @@
-/*! Copyright (c) 2012 Joshfire - MIT license */
-/**
- * @fileoverview Core of the JSON Form client-side library.
- *
- * Generates an HTML form from a structured data model and a layout description.
- *
- * The library may also validate inputs entered by the user against the data model
- * upon form submission and create the structured data object initialized with the
- * values that were submitted.
- *
- * The library depends on:
- *  - jQuery
- *  - the underscore library
- *  - a JSON parser/serializer. Nothing to worry about in modern browsers.
- *  - the JSONFormValidation library (in jsv.js) for validation purpose
- *
- * See documentation at:
- * http://developer.joshfire.com/doc/dev/ref/jsonform
- *
- * The library creates and maintains an internal data tree along with the DOM.
- * That structure is necessary to handle arrays (and nested arrays!) that are
- * dynamic by essence.
- */
-
 /*global window*/
 
 /**
