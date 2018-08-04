beda9f5dd7519bd9afc8256228f080a1d113db5e
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2018-08-04 19:42:53 +1000

    refactor(test): remove old test files

--- src/__tests__/jsonform.spec.ts
+++
@@ -1,4 +0,0 @@
-it('runs function', function(){
-	let r = 1 + 2;
-	expect(r).toEqual(3);
-});
--- src/test/references.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../build/jsonform.d.ts" />
-/// <reference path="../references.d.ts" />
-
-interface JQuery {
-    jsonForm: (options, param1?) => any;
-}
-
-declare function fetch(input, options?);
